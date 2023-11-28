637f83a982addae577bc5549bc48d63d0a13647c
marcodci <118737381+marcodci@users.noreply.github.com>
2023-11-28 11:47:08 +0100

    Update 03-js-doc-callback.js

--- 03-js-doc-callback.js
+++ 03-js-doc-callback.js
@@ -1,42 +1,39 @@
-// FUNCTION CALLBACK
-
 /**
- * @type {number[]} - array of numbers
+ * Example data: an array of numbers.
+ * @type {number[]}
  */
-let data = [1,2,3,4,5]
-
-
-
-/**
- * This callback is invoked after data processing.
- * 
- * @callback myCallback - callback function
- * @param {number[]} processedData - array of numbers
- * 
-*/
-
-
-function myCallback(processedData) { // gives "any" because the IDE probably has a limitation with JsDoc
-    console.log("Processed data:", processedData)
-}
+let data = [1, 2, 3, 4, 5];
 
 
 /**
- * Process an array of data and then executes a callback function
- * 
- * @param {number[]} data - The data to be processed.
- * @param {processData} callback - the callback to execute after processing
- * 
-*/
-function processData(data, callback){
-    // multiply each element of array data by 10
-    let updatedArray = data.map(item => item * 10)
+ * Callback function that is invoked after data processing.
+ * @callback myCallback
+ * @param {number[]} processedData - An array of numbers
+ */
 
-    //invoke the callback function
-    callback(updatedArray)
+/**
+ * Callback function to log the processed data.
+ * 
+ * @param {number[]} processedData - An array of numbers
+ */
+function myCallback(processedData) {
+    console.log("Processed data:", processedData);
+}
+
+/**
+ * Process an array of data and then execute a callback function.
+ * 
+ * @param {number[]} data - The data to be processed
+ * @param {myCallback} callback - The callback to execute after processing
+ */
+function processData(data, callback) {
+    // Multiply each element of the data array by 10
+    let updatedArray = data.map(item => item * 10);
+
+    // Invoke the callback function with the updated data
+    callback(updatedArray);
 }
 
 
-
-// Call processData, passing both data array and the callback function
-processData(data, myCallback)
+// Process the data and call the callback
+processData(data, myCallback);
